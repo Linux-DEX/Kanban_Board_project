3dc70ef2c17cefcb8de358ff82622350e2cc6a6e
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-26 19:03:57 +0530

    Kanban columns

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,7 +1,76 @@
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import { Column } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
 const KanbanBoard = () => {
+  const [columns, setColumns] = useState<Column[]>([]);
+
+  console.log(columns);
+
   return (
-    <div>KanbanBoard</div>
-  )
+    <div
+      className="
+          m-auto
+          flex
+          min-h-screen
+          w-full
+          items-center
+          overflow-x-auto
+          overflow-y-hidden
+          px-[40px]
+      "
+    >
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns.map((col) => (
+            <ColumnContainer column={col} deleteColumn={deleteColumn} />
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            createNewColumn();
+          }}
+          className="
+        h-[60px]
+        w-[350px]
+        min-w-[350px]
+        cursor-pointer
+        rounded-lg
+        bg-mainBackgroundColor
+        border-2
+        border-columnBackgroundColor
+        p-4
+        ring-rose-500
+        hover:ring-2
+        flex
+        gap-2
+          "
+        >
+          <PlusIcon />
+          Add Column
+        </button>
+      </div>
+    </div>
+  );
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumns([...columns, columnToAdd]);
+  }
+
+  function deleteColumn(id: Id) {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  }
+};
+
+function generateId() {
+  return Math.floor(Math.random() * 10001);
 }
 
-export default KanbanBoard
+export default KanbanBoard;
